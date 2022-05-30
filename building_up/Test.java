fbe60c3e53bb480d6c3563463c34daa2e6aa75a7
Emma Petrie <97540217+emmapetrie8@users.noreply.github.com>
2022-05-30 22:40:24 +0100

    Create Test.java

---
+++ building_up/Test.java
@@ -0,0 +1,47 @@
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+
+public class Test {
+	//need the array List of shows
+	ArrayList<String> shows = new ArrayList<>();
+	Random rand = new Random();
+	
+	public Test() {
+		
+	}
+	
+	public void readFile() throws IOException {
+		
+		BufferedReader bufReader = new BufferedReader(new FileReader("/Users/emmapetrie/genre_recomendations/horror.txt")); 
+		ArrayList<String> shows = new ArrayList<>(); 
+		String line = bufReader.readLine(); 
+		while (line != null) { 
+			shows.add(line); 
+			line = bufReader.readLine(); 
+		} 
+		bufReader.close();
+		this.shows = shows;
+
+	}
+	
+	public void selectShow() {
+		int size = shows.size();
+		Random rand = new Random();
+		int int_random = rand.nextInt(size);
+				
+		String recomendatedShow = shows.get(int_random); 
+		System.out.println("The randomly selected recomendation is: " + recomendatedShow + " enjoy!");
+
+	}
+	
+	public static void main (String args[]) throws IOException {
+		Test test = new Test();
+		test.readFile();
+		test.selectShow();
+	}
+
+}
