c85065fbfe9fc32c2b45e869c24f9325f5c35ac4
Emma Petrie <97540217+emmapetrie8@users.noreply.github.com>
2022-05-30 22:41:00 +0100

    second test, added user input

---
+++ building_up/Test2.java
@@ -0,0 +1,61 @@
+package project3;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Random;
+import java.util.Scanner;
+
+import project.genres;
+
+public class Test2 {
+	//need the array List of shows
+	ArrayList<String> shows = new ArrayList<>();
+	Random rand = new Random();
+	String genre;
+	
+	public Test2() {
+		
+	}
+	
+	public void getGenreInput() {
+		Scanner genreInput = new Scanner(System.in);  // Create a Scanner object
+	    System.out.println("Please entre the genre you want recomendations for");
+
+	    String genre = genreInput.nextLine();  // Read user input
+	    System.out.println("the genre for recomendations is: " + genre);  // Output user input
+	    this.genre = genre;	    
+	}
+	
+	public void readFile() throws IOException {
+		BufferedReader bufReader = new BufferedReader(new FileReader("/Users/emmapetrie/genre_recomendations/horror.txt")); 
+		ArrayList<String> shows = new ArrayList<>(); 
+		String line = bufReader.readLine(); 
+		while (line != null) { 
+			shows.add(line); 
+			line = bufReader.readLine(); 
+		} 
+		bufReader.close();
+		this.shows = shows;
+
+	}
+	
+	public void selectShow() {
+		int size = shows.size();
+		Random rand = new Random();
+		int int_random = rand.nextInt(size);
+				
+		String recomendatedShow = shows.get(int_random); 
+		System.out.println("The randomly selected recomendation is: " + recomendatedShow + " enjoy!");
+
+	}
+	
+	public static void main (String args[]) throws IOException {
+		Test2 test = new Test2();
+		test.getGenreInput();
+		test.readFile();
+		test.selectShow();
+	}
+
+}
